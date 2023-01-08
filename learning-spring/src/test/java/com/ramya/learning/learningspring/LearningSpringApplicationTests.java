0995a8a42e9f6cd92182ae6c17ee05ed631e560f
Ramya Non-IU <ramyaram2092@outlook.com>
2023-01-08 00:04:02 -0500

    Initial commit

---
+++ learning-spring/src/test/java/com/ramya/learning/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramya.learning.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
